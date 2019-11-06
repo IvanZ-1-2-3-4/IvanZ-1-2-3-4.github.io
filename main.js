c67ab7d1d81fb9051bee7ec93254c35303f8e099
Ivan Zabrodin <pyrat32@gmail.com>
2019-11-06 13:28:33 -0600

    lazy message

--- main.js
+++ main.js
@@ -3,11 +3,21 @@ $(document).ready(function() {
 
     let timeInt = 20;
 
-    let widthReal = 100;
-    let heightReal = 50;
+    let start = null;
+    let previousTimeStamp;
+
+    let widthReal = 50;
+    let heightReal = 25;
     let time = 0;
     const canvasArea = 1000 * 500;
     let isPlaying = false;
+
+    // Get canvas width in pixels
+    let str = $("#main-canvas").attr("width");
+    let canvasWidth = parseInt(str.substring(0, str.length - 1));
+    str = $("#main-canvas").attr("height");
+    let canvasHeight = parseInt(str.substring(0, str.length - 1));  
+    console.log(canvasWidth);console.log(canvasHeight);
 
     let posXDisplay = document.getElementById("posx");
     let posYDisplay = document.getElementById("posy");
@@ -28,7 +38,7 @@ $(document).ready(function() {
         previousY: 0,
         // Ball's velocity in meters per second
         velX: 0,
-        velY: 50,
+        velY: 0,
         // Magnitude of ball's velocity
         speed: 0,
         // Direction of the ball's movement in radians
@@ -52,15 +62,23 @@ $(document).ready(function() {
             context.fillStyle = "#000000";
             context.fill();
 
+            if (document.getElementById("trail").value == "trail") {
+                context.beginPath();
+                context.arc(this.posX, this.posY, 1, 0, 2 * Math.PI);
+                context.fill();
+            }
+
             // Update previous coordinates
             this.previousX = this.posX;
             this.previousY = this.posY;
         }
     };
-    console.log(ball.posRealX);
     ball.draw();
 
-    $("#play").click(function() {runSimulation();}); // This is so retarded oh so retarded
+    $("#play").click(function() {
+        isPlaying = true;
+        requestAnimationFrame(runSimulation);
+    }); // This is so retarded oh so retarded
 
     $("#set-dimensions").click(function() {
         widthReal = parseInt(document.getElementById("widthin").value);
@@ -68,57 +86,68 @@ $(document).ready(function() {
         $("#main-canvas").attr("width", Math.sqrt(canvasArea * widthReal / heightReal));
         $("#main-canvas").attr("height", Math.sqrt(canvasArea * heightReal / widthReal));
         ball.posRealY = heightReal / 2;
+        
+        let str = $("#main-canvas").attr("width");
+        canvasWidth = parseInt(str.substring(0, str.length - 1));
+        str = $("#main-canvas").attr("height");
+        canvasHeight = parseInt(str.substring(0, str.length - 1));  
+
         ball.draw();
     });
 
-    function runSimulation() {
-        isPlaying = true;
+    function runSimulation(timestamp) {
+        if (!start) {
+            start = timestamp;
+            previousTimeStamp = start;
+        }
 
-        // Get canvas width in pixels
-        let str = $("#main-canvas").attr("width");
-        let canvasWidth = parseInt(str.substring(0, str.length - 1));
-        str = $("#main-canvas").attr("height");
-        let canvasHeight = parseInt(str.substring(0, str.length - 1));  
+        // Increment time
+        time = (timestamp - start) / 1000;
+        console.log("time: " + parseInt(time));
+        console.log("dif: " + (timestamp - previousTimeStamp));
 
-        // Animation loop
-        let play = setInterval(function() {
+        // Adjust y-velocity and real y-position
+        if (ball.posY > canvasHeight - ball.width) {
+            // If ball hits the ground, reverse direction
+            ball.velY = -ball.velY * 0.5;
+            ball.posRealY = -(canvasHeight - ball.width) * heightReal / canvasHeight;
+            console.log("GHLEOO: " + time);
+        } else {
+            // Accelerate downwards by 9.81 meters per second squared
+            ball.velY = ball.velY - (0.5 * 9.81 * Math.pow((timestamp - previousTimeStamp) / 1000), 2);
+            ball.posRealY = (ball.posRealY + ball.velY);
+        }
 
-            // Adjust y-velocity and real y-position
-            if (ball.posY > canvasHeight - ball.width) {
-                // If ball hits the ground, reverse direction
-                ball.velY = -ball.velY * 0.5;
-                ball.posRealY = -(canvasHeight - ball.width) * heightReal / canvasHeight;
-            } else {
-                // Accelerate downwards by 9.81 meters per second squared
-                ball.velY = ball.velY - (9.81 * timeInt / 1000);
-                ball.posRealY = (ball.posRealY + ball.velY) * timeInt / 1000;
-            }
+        updateSpeed();
 
-            updateSpeed();
+        // Update ball's real position
+        ball.posRealX = ball.posRealX + ball.velX;
+        console.log("real: " + ball.posRealX + "," + ball.posRealY);
+        console.log("vel: " + ball.velY);
 
-            // Update ball's real position
-            ball.posRealX = ball.posRealX + ball.velX;
-            console.log("real: " + ball.posRealX + "," + ball.posRealY);
+        // Convert real position to pixel position
+        ball.posX = ball.posRealX * canvasWidth / widthReal;
+        ball.posY = -ball.posRealY * canvasHeight / heightReal;
 
-            // Convert real position to pixel position
-            ball.posX = ball.posRealX * canvasWidth / widthReal;
-            ball.posY = -ball.posRealY * canvasHeight / heightReal;
+        // Draw the frame
+        ball.draw();
+        // Output ball stats
+        printData();
+        previousTimeStamp = timestamp;
 
-            // Draw the frame
-            ball.draw();
-            // Output ball stats
-            printData();
+        if (isPlaying == true) {
+            requestAnimationFrame(runSimulation);
+        }
 
-            // End loop if paused
-            $("#pause").click(function() {
-                clearInterval(play);
-                isPlaying = false;
-            });
-        }, timeInt);
+        // End loop if paused
+        $("#pause").click(function() {
+            isPlaying = false;
+            start = null;
+        });
     }
 
     function updateSpeed() {
-        ball.speed = Math.sqrt(Math.pow(ball.velX, 2) + Math.pow(ball.velY, 2));
+        ball.speed = Math.sqrt(Math.pow(ball.velX, 2) + Math.pow(ball.velY, 2)); // √(a²+b²)
         ball.angle = Math.atan(ball.velY / ball.velX);
     }
 
